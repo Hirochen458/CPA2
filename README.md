39c6ae57daea8978e1f31ea0e8bbbc7e2ffd8c69
Hiro Chen <hirochen@macbook-pro-3.myfiosgateway.com>
2022-05-09 05:08:51 -0400

    added

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # CPA2
+
+
+Brandeis University: COSI 103A - Fundamentals of Software Engineering
+
+ This repository was created for class projects/assignments. It will primarily house the file contents of the CPA02 assignment, which contains a dataset of Brandeis course selections, professors, and etc.
+ This repository was created for class projects/assignments. It will primarily house the file contents of the CPA02 assignment, which contains a dataset of Brandeis course selections, professors, and etc.
+
+ Hiro Chen
+ 
+## Installation
+
+Download the project from github and download nodejs and npm from https://nodejs.org
+and cd into the folder
+
+Install the packages with
+``` bash
+npm install
+```
+Start the project with
+``` bash
+node app.js
+```
+or install nodemon (the node monitoring app) with
+``` bash
+npm install -g nodemon
+```
+and start the project with
+``` bash
+nodemon
+```
+/var/folders/28/ypwn7n8s5vs57d3c3wbhrd2m0000gn/T/TemporaryItems/NSIRD_screencaptureui_6Jyzf5/截屏2022-05-09 上午4.45.54.png
+That is a picture of my website, it is mainly used to log the places you want to go in the future.
---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const bodyPayser = require('body-parser');
+const app = express();
+var MongoClient = require('mongodb').MongoClient
+
+app.listen(3000,function(){
+        console.log('listening on 3000')
+    })
+
+app.use(bodyPayser.urlencoded({extended: true}))
+app.use(bodyPayser.json())
+app.use(express.static('public'))
+
+URL = 'mongodb+srv://Hiro:1q2w3e@cluster0.lrztk.mongodb.net/CPA2retryWrites=true&w=majority'
+MongoClient.connect(URL, {useUnifiedTopology: true}).then(client => {
+    console.log('connected to DataBase')
+
+    const db = client.db('CPA2')
+    const quotesCollection = db.collection('quotes')
+    app.set('view engine', 'ejs')
+
+    app.post('/quotes', (req, res)=>{
+        quotesCollection.insertOne(req.body).then(result =>{res.redirect('/')})
+    })
+
+    app.get('/', (req,res)=>{
+        db.collection('quotes').find().toArray()
+        .then(results => {
+        res.render('index.ejs',{quotes:results})
+        })
+        .catch(error => console.error(error))
+        //res.sendFile(__dirname + '/index.html')
+
+    })
+
+
+    app.put('/quotes', (req, res) => {
+        quotesCollection.findOneAndUpdate(
+          { name: 'zhang' },
+          {
+            $set: {
+              name: req.body.name,
+              place: req.body.quote
+            }
+          },
+          {
+            upsert: true
+          }
+        )
+          .then(result => res.json('Success'))
+          .catch(error => console.error(error))
+      })
+
+      app.delete('/quotes', (req, res) => {
+        quotesCollection.deleteOne(
+          { name: req.body.name }
+        )
+          .then(result => {
+            if (result.deletedCount === 0) {
+              return res.json('No quote to delete')
+            }
+            res.json('Deleted Darth Vadar\'s quote')
+          })
+          .catch(error => console.error(error))
+      })
+
+    })
+
+
+
+
+
+
+
+
+//
+
+
+
+
+
+
+
+//})
